468b1dd836655fcdef8daf16f6ddedf126589541
Mehak Vohra <watthemehak@gmail.com>
2024-04-18 12:50:11 -0700

    day 17

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,12 +4,15 @@ import girlducky from "../images/girl_ducky.webp";
 function Home() {
   return (
     <>
-      <Navbar>
+      <Navbar expand="lg" className="bg-body-tertiary">
         <Container>
           <Navbar.Brand>Girl Meets Web</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse>
             <Nav>
-              <Nav.Link />
+              <Nav.Link href="#">Home</Nav.Link>
+              <Nav.Link href="#">About</Nav.Link>
+              <Nav.Link href="#">Contact Us</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
