58ec8e75159b972b486970390442b3d459f1163f
Mehak Vohra <watthemehak@gmail.com>
2024-04-20 19:30:22 -0700

    day 19

--- src/components/deck.js
+++ src/components/deck.js
@@ -23,4 +23,13 @@ function createDeck() {
       deck.push({ suit, values });
     }
   }
+  return deck;
+}
+
+function shuffleDeck(deck) {
+  for (let i = deck.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [deck[i], deck[j]] = [deck[j], deck[i]];
+  }
+  return deck;
 }
---
+++ src/components/table.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Table = styled.div`
+  position: absolute;
+  width: 65%;
+  height: 45%;
+  left: 50%;
+  top: 25%;
+  transform: translate(-50%);
+  text-align: center;
+  z-index: -1;
+  background: #003a62;
+  border: 10px solid rgb(196, 196, 196);
+  border-radius: 200px;
+`;
